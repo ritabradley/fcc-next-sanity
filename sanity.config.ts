a19d5113770c7fae42e9d20db7594a929e06c5d2
Rita Bradley <rita@ritabradley.dev>
2023-04-08 18:58:44 -0700

    ✨ setup `projects` schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'sanity';
-import { deskTool } from 'sanity/desk'
+import { deskTool } from 'sanity/desk';
+import schemas from './sanity/schemas';
 
 const config = defineConfig({
-    projectId: "4h6y9z76",
-    dataset: "production",
-    title: "My Personal Sanity Site",
-    apiVersion: "2023-04-08",
-    basePath: "/admin",
-    plugins: [deskTool()]
-})
+    projectId: '4h6y9z76',
+    dataset: 'production',
+    title: 'My Personal Sanity Site',
+    apiVersion: '2023-04-08',
+    basePath: '/admin',
+    plugins: [deskTool()],
+    schema: { types: schemas },
+});
 
 export default config;
