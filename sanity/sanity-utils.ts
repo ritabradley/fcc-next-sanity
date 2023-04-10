4472b93767b226ce9cd180b3bb406a6017d0134d
Rita Bradley <rita@ritabradley.dev>
2023-04-09 22:21:59 -0700

    ✨ create `getProject` utility function

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -19,3 +19,25 @@ export const getProjects = async (): Promise<Project[]> => {
         content,
     }`);
 };
+
+export async function getProject(slug: string): Promise<Project> {
+    const client = createClient({
+        projectId: '4h6y9z76',
+        dataset: 'production',
+        apiVersion: '2023-04-08',
+    });
+
+    return client.fetch(
+        groq`*[ _type == "project" && slug.current == $slug][0]{
+        _id,
+        _createdAt,
+        name,
+        "slug": slug.current,
+        "image": image.asset->url,
+        alt,
+        url,
+        content,
+    }`,
+        { slug }
+    );
+}
